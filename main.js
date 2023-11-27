c560c525b1e27f705da17ceab9cc435024df316f
Bruno <brunoamarante83@gmail.com>
2023-11-27 20:10:09 -0300

    commit Inicial

---
+++ main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('.counter').each(function() {
+        $(this).prop('counter',0).animate({
+            counter: $(this).text()
+
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
+
